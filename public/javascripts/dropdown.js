d1f1d48aeeb1f1f04acd0dbd8e551c7d0017d41e
vitu_77 <marcelo.victor05@gmail.com>
2019-11-22 21:25:57 -0300

    dropdown faq improved

--- public/javascripts/dropdown.js
+++ public/javascripts/dropdown.js
@@ -4,15 +4,21 @@ const plusIcons = document.querySelectorAll('.plus-icon .y');
 
 rowsHeader[0].addEventListener('click', () => {
     dropdownItems[0].classList.toggle('hide-dropdown');
+    dropdownItems[1].classList.add('hide-dropdown');
+    dropdownItems[2].classList.add('hide-dropdown');
     plusIcons[0].classList.toggle('change-icon');
 });
 
 rowsHeader[1].addEventListener('click', () => {
     dropdownItems[1].classList.toggle('hide-dropdown');
+    dropdownItems[0].classList.add('hide-dropdown');
+    dropdownItems[2].classList.add('hide-dropdown');
     plusIcons[1].classList.toggle('change-icon');
 });
 
 rowsHeader[2].addEventListener('click', () => {
     dropdownItems[2].classList.toggle('hide-dropdown');
+    dropdownItems[0].classList.add('hide-dropdown');
+    dropdownItems[1].classList.add('hide-dropdown');
     plusIcons[2].classList.toggle('change-icon');
 });
