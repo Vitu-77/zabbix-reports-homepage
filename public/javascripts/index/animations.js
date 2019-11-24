a486c0c35d5e26c89512af2ebbbf58437eceb497
vitu_77 <marcelo.victor05@gmail.com>
2019-11-24 20:20:55 -0300

    animations implementeds

---
+++ public/javascripts/index/animations.js
@@ -0,0 +1,109 @@
+const get = (reference) => document.querySelector(reference);
+const getAll = (reference) => document.querySelectorAll(reference);
+
+const animate = () => {
+    const features = get('section#about');
+    const featuresOffsetTop = Math.floor(features.offsetTop * 0.5);
+
+    const prices = get('section#prices');
+    const pricesOffsetTop = Math.floor(prices.offsetTop * 0.65);
+
+    const videos = get('section#video');
+    const videosOffsetTop = Math.floor(videos.offsetTop * 0.76);
+
+    const contact = get('section#contact');
+    const contactOffsetTop = Math.floor(contact.offsetTop * 0.8);
+
+    if (window.scrollY >= featuresOffsetTop) {
+
+        const featuresTitle = get('.features .subtitle-container h2');
+        const featureCards = getAll('.feature-cards article');
+
+        const aboutTimeline = new TimelineMax();
+
+        aboutTimeline
+            .to(featuresTitle, .4, {
+                transform: 'translateX(0)',
+                opacity: 1
+            })
+            .to(featureCards[0], .4, {
+                transform: 'translateY(0)'
+            })
+            .to(featureCards[1], .4, {
+                opacity: 1
+            })
+            .to(featureCards[2], .2, {
+                transform: 'translateY(0)'
+            })
+    }
+
+    if (window.scrollY >= pricesOffsetTop) {
+        const startFree = get('article.prices .subtitle-container h2 span#fc');
+        const payOnly = get('article.prices .subtitle-container h2 span#lc');
+        const priceCards = get('.price-cards');
+
+        const pricesTimeline = new TimelineMax();
+
+        pricesTimeline
+            .to(startFree, 1, { opacity: 1 })
+            .to(payOnly, .4, { opacity: 1 })
+            .to(priceCards, .4, {
+                width: '100%',
+                opacity: 1,
+            })
+
+    }
+
+    if (window.scrollY >= videosOffsetTop) {
+        const videoTitle = get('article.demo-video .subtitle-container h2');
+        const video = get('.video-container video');
+
+        const videoTimeline = new TimelineMax();
+
+        videoTimeline
+            .to(videoTitle, .8, { opacity: 1 })
+            .to(video, 1, { transform: 'translateX(0)' }, { easy: Power2.easyInOut })
+    }
+
+    if (window.scrollY >= contactOffsetTop) {
+        const faq = get('article.faq');
+        const form = get('.contact-form form');
+        const formTitle = get('.contact-form h2');
+        const formSubtitle = get('.contact-form h3');
+        const formButton = get('.contact-form button');
+
+        const contactTimeline = new TimelineMax();
+
+        contactTimeline
+            .to(faq, .6, { transform: 'translateX: (0)' })
+            .to(formTitle, .4, { opacity: 1 })
+            .to(formSubtitle, .4, { opacity: 1 })
+            .to(form, .4, { opacity: 1 })
+            .to(formButton, .4, { opacity: 1 })
+    }
+}
+
+window.onload = () => {
+    const logo = get('div.logo');
+    const menu = get('header nav');
+    const homeContentTitle = get('div.text-content h2');
+    const homeContentParagraph = get('div.text-content p');
+    const homeContentButton = get('div.text-content a');
+    const homeContentImg = get('div.home-content-right img');
+
+    const homeTileline = new TimelineMax();
+
+    homeTileline
+        .to(logo, .4, { transform: 'translateX(0)' })
+        .to(menu, .4, { transform: 'translateX(0)' })
+        .to(homeContentTitle, .2, { opacity: 1 })
+        .to(homeContentParagraph, .2, { opacity: 1 })
+        .to(homeContentButton, .2, { marginLeft: 0 })
+        .to(homeContentImg, 1.2, { opacity: 1 });
+
+    animate();
+}
+
+window.addEventListener('scroll', () => {
+    animate();
+});
